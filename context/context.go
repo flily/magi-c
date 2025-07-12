7241e4d8e31f7173bf2e6ef2298cfb998babbeb5
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-07-12 11:50:15 +0800

    add highlight constructor

--- context/context.go
+++ context/context.go
@@ -1,8 +1,26 @@
 package context
 
+import (
+	"fmt"
+)
+
 type Highlight struct {
 	Start int
 	End   int
+}
+
+func NewHighlight(start, end int) *Highlight {
+	if start < 0 || end < 0 || start > end {
+		err := fmt.Errorf("invalid highlight range: start=%d, end=%d", start, end)
+		panic(err)
+	}
+
+	h := &Highlight{
+		Start: start,
+		End:   end,
+	}
+
+	return h
 }
 
 type LineContext struct {
