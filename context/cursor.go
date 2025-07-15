3ba3732cc840de918ae3ce61ab62760d8f37b4ae
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-07-15 21:10:46 +0800

    fix cursor, fix next, now do not return new cursor instance

--- context/cursor.go
+++ context/cursor.go
@@ -7,25 +7,29 @@ type Cursor struct {
 	LineContext *LineContext
 }
 
-func (c *Cursor) Next() *Cursor {
+func (c *Cursor) Rune() (rune, bool) {
+	if c.Line >= len(c.File.Contents) {
+		return 0, true
+	}
+
+	// Cursor will never stop on a column that is out of bounds
+	l := c.File.LineContent(c.Line)
+	return l.Content[c.Column], false
+}
+
+func (c *Cursor) moveNext(line int, column int) (int, int, *LineContext) {
 	lctx := c.LineContext
-	line := c.Line
-	column := c.Column + 1
-	for column >= c.LineContext.Length() {
+	column += 1
+	for column >= lctx.Length() {
 		line += 1
 		lctx = c.File.LineContext(line)
 		if lctx == nil {
-			return nil
+			return line, 0, nil
 		}
 		column = 0
 	}
 
-	return &Cursor{
-		Line:        line,
-		Column:      column,
-		File:        c.File,
-		LineContext: lctx,
-	}
+	return line, column, lctx
 }
 
 func (c *Cursor) Peek() (rune, bool) {
@@ -39,4 +43,33 @@ func (c *Cursor) Peek() (rune, bool) {
 	}
 
 	return l.Content[c.Column], false
+}
+
+func (c *Cursor) Next() (rune, bool) {
+	line, column, lctx := c.moveNext(c.Line, c.Column)
+	if lctx == nil {
+		return 0, true
+	}
+
+	r := lctx.Content.Content[column]
+	c.LineContext = lctx
+	c.Line = line
+	c.Column = column
+	return r, false
+}
+
+func (c *Cursor) NextCursor() *Cursor {
+	next := &Cursor{
+		Line:        c.Line,
+		Column:      c.Column,
+		File:        c.File,
+		LineContext: c.LineContext,
+	}
+
+	_, eof := next.Next()
+	if eof {
+		return nil
+	}
+
+	return next
 }
