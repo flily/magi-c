e24a452e39791dec717c6dbbb0fc4000ed8e0198
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-09-24 23:59:11 +0800

    add terminal nodes structure

--- ast/literals.go
+++ ast/literals.go
@@ -1,6 +1,8 @@
 package ast
 
-import "github.com/flily/magi-c/context"
+import (
+	"github.com/flily/magi-c/context"
+)
 
 type StringLiteral struct {
 	TerminalNode
@@ -14,4 +16,51 @@ func NewStringLiteral(ctx *context.Context, value string) *StringLiteral {
 	}
 
 	return l
+}
+
+type IntegerLiteral struct {
+	TerminalNode
+	Type  BaseType
+	Sign  int
+	Value uint64
+}
+
+func NewIntegerLiteral(ctx *context.Context, sign int, value uint64) *IntegerLiteral {
+	l := &IntegerLiteral{
+		TerminalNode: NewTerminalNode(ctx),
+		Sign:         sign,
+		Value:        value,
+	}
+
+	return l
+}
+
+type FloatLiteral struct {
+	TerminalNode
+	Sign  int
+	Value float64
+}
+
+func NewFloatLiteral(ctx *context.Context, sign int, value float64) *FloatLiteral {
+	l := &FloatLiteral{
+		TerminalNode: NewTerminalNode(ctx),
+		Sign:         sign,
+		Value:        value,
+	}
+
+	return l
+}
+
+type Identifier struct {
+	TerminalNode
+	Name string
+}
+
+func NewIdentifier(ctx *context.Context, name string) *Identifier {
+	id := &Identifier{
+		TerminalNode: NewTerminalNode(ctx),
+		Name:         name,
+	}
+
+	return id
 }
