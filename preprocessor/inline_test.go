3a7926f9c005856751543537bf3ecf622104b6e4
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-11-01 14:18:36 +0800

    add test case of empty inline block

--- preprocessor/inline_test.go
+++ preprocessor/inline_test.go
@@ -24,11 +24,11 @@ func TestInlineDirectiveBasic(t *testing.T) {
 
 	result, ok := node.(*ast.PreprocessorInline)
 	if !ok {
-		t.Fatalf("expected PreprocessorInline node, got %T", node)
+		t.Fatalf("expect PreprocessorInline node, got %T", node)
 	}
 
 	if result == nil {
-		t.Fatalf("expected non-nil PreprocessorInline node")
+		t.Fatalf("expect non-nil PreprocessorInline node")
 	}
 
 	gotContent := result.Content.HighlightText("here")
@@ -40,6 +40,36 @@ func TestInlineDirectiveBasic(t *testing.T) {
 		"        here",
 	}, "\n")
 	if gotContent != expContent {
-		t.Errorf("expected content context highlight:\n%s\ngot:\n%s", expContent, gotContent)
+		t.Errorf("expect content context highlight:\n%s\ngot:\n%s", expContent, gotContent)
+	}
+}
+
+func TestInlineDirectiveWithEmptyBlock(t *testing.T) {
+	code := strings.Join([]string{
+		"#inline c",
+		"#end-inline c",
+	}, "\n")
+
+	cursor := context.NewCursorFromString("example.c", code)
+	node, err := scanDirectiveOn(cursor, Inline)
+	if err != nil {
+		t.Fatalf("unexpected error:\n%v", err)
+	}
+
+	result, ok := node.(*ast.PreprocessorInline)
+	if !ok {
+		t.Fatalf("expect PreprocessorInline node, got %T", node)
+	}
+
+	if result == nil {
+		t.Fatalf("expect non-nil PreprocessorInline node")
+	}
+
+	if result.Content != nil {
+		t.Errorf("expect nil content context for empty inline block, got non-nil")
+	}
+
+	if !result.Empty() {
+		t.Errorf("expect empty node returned")
 	}
 }
