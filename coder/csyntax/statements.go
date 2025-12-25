32dc2843db7dcbc69a1241cf1d15ed94f32b7650
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-12-25 11:02:36 +0800

    add assignment statement and test cases

--- coder/csyntax/statements.go
+++ coder/csyntax/statements.go
@@ -1,1 +1,95 @@
 package csyntax
+
+import (
+	"strings"
+)
+
+type BlockContext int
+
+const (
+	BlockContextFunction BlockContext = iota
+	BlockContextIf
+	BlockContextElse
+	BlockContextDo
+	BlockContextWhile
+	BlockContextFor
+	BlockContextSwitch
+	BlockContextCase
+	BlockContextDefault
+)
+
+type CodeBlock struct {
+	Statements []Statement
+}
+
+func NewCodeBlock(statements []Statement) *CodeBlock {
+	b := &CodeBlock{
+		Statements: statements,
+	}
+
+	return b
+}
+
+func (b *CodeBlock) statementNode() {}
+
+func (b *CodeBlock) Write(out *StyleWriter, level int) error {
+	if err := out.WriteIndent(level); err != nil {
+		return err
+	}
+
+	if err := out.WriteLine(LeftBrace); err != nil {
+		return err
+	}
+
+	for _, stmt := range b.Statements {
+		if err := stmt.Write(out, level+1); err != nil {
+			return err
+		}
+	}
+
+	if err := out.WriteIndent(level); err != nil {
+		return err
+	}
+
+	return out.WriteLine(RightBrace)
+}
+
+type AssignmentStatement struct {
+	LeftIdentifier   string
+	LeftPointerLevel int
+	RightExpression  Expression
+}
+
+func NewAssignmentStatement(leftIdentifier string, leftPointerLevel int, rightExpression Expression) *AssignmentStatement {
+	s := &AssignmentStatement{
+		LeftIdentifier:   leftIdentifier,
+		LeftPointerLevel: leftPointerLevel,
+		RightExpression:  rightExpression,
+	}
+
+	return s
+}
+
+func (s *AssignmentStatement) statementNode() {}
+
+func (s *AssignmentStatement) Write(out *StyleWriter, level int) error {
+	if err := out.WriteIndent(level); err != nil {
+		return err
+	}
+
+	pointer := strings.Repeat(PointerAsterisk, s.LeftPointerLevel)
+	pointerSpace := ""
+	if s.LeftPointerLevel > 0 && out.style.PointerSpacingBefore {
+		pointerSpace = Space
+	}
+
+	if err := out.Write("%s%s%s%s", pointer, pointerSpace, s.LeftIdentifier, out.style.Assign()); err != nil {
+		return err
+	}
+
+	if err := s.RightExpression.Write(out, level); err != nil {
+		return err
+	}
+
+	return out.WriteLine(Semicolon)
+}
---
+++ coder/csyntax/statements_test.go
@@ -0,0 +1,42 @@
+package csyntax
+
+import (
+	"testing"
+)
+
+func TestAssignmentStatementOnNormalVariable(t *testing.T) {
+	stat := NewAssignmentStatement("a", 0, NewIntegerLiteral(10))
+
+	var _ Statement = stat
+	stat.statementNode()
+
+	builder, writer := makeTestWriter(testStyle1)
+	err := stat.Write(writer, 0)
+	if err != nil {
+		t.Fatalf("AssignmentStatement Write failed: %s", err)
+	}
+
+	expected := "a = 10;\n"
+	result := builder.String()
+	if result != expected {
+		t.Fatalf("AssignmentStatement Write result wrong:\nexpected:\n%s\ngot:\n%s", expected, result)
+	}
+}
+
+func TestAssignmentStatementOnPointerVariable(t *testing.T) {
+	stat := NewAssignmentStatement("p", 1, NewIntegerLiteral(20))
+	var _ Statement = stat
+	stat.statementNode()
+
+	builder, writer := makeTestWriter(testStyle1)
+	err := stat.Write(writer, 0)
+	if err != nil {
+		t.Fatalf("AssignmentStatement Write failed: %s", err)
+	}
+
+	expected := "*p = 20;\n"
+	result := builder.String()
+	if result != expected {
+		t.Fatalf("AssignmentStatement Write result wrong:\nexpected:\n%s\ngot:\n%s", expected, result)
+	}
+}
