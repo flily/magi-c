4cc32de6cfb3854ed613ed9f5591f72d9ca31a59
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-02-10 16:02:11 +0800

    fix main entry structure, add sub-commands

--- cmd/magi-c/main.go
+++ cmd/magi-c/main.go
@@ -3,24 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"path"
 
 	"github.com/flily/magi-c/coder"
 	"github.com/flily/magi-c/context"
 )
 
-func main() {
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) <= 0 {
-		flag.Usage()
-		return
-	}
-
-	filename := args[0]
-
-	c := coder.NewCoder(".", "output")
+func translateFile(c *coder.Coder, filename string) error {
 	err := c.ParseFile(filename)
 	if err != nil {
 		switch e := err.(type) {
@@ -31,13 +21,13 @@ func main() {
 			fmt.Printf("Error:\n%s\n", err)
 		}
 
-		return
+		return err
 	}
 
 	err = c.Check(filename)
 	if err != nil {
 		fmt.Printf("Error:\n%s\n", err)
-		return
+		return err
 	}
 
 	outputFilename := path.Base(coder.OutputFilename(filename))
@@ -46,8 +36,77 @@ func main() {
 	if err != nil {
 		fmt.Printf("    failed\n")
 		fmt.Printf("Error:\n%s\n", err)
-		return
+		return err
 	}
 
 	fmt.Printf("    ok\n")
+	return nil
+}
+
+func doTranslate(args []string) error {
+	set := flag.NewFlagSet("translate", flag.ExitOnError)
+	source := set.String("source", ".", "source base directory")
+	output := set.String("output", "output", "output base directory")
+	_ = set.Parse(args)
+
+	c := coder.NewCoder(*source, *output)
+
+	base := "."
+
+	if set.NArg() > 0 {
+		base = set.Arg(0)
+	}
+
+	stat, err := os.Stat(base)
+	if err != nil {
+		return err
+	}
+
+	if stat.IsDir() {
+
+	} else {
+		err = translateFile(c, base)
+	}
+
+	return err
+}
+
+func doBuild(args []string) error {
+	return nil
+}
+
+var commandMap = map[string]func([]string) error{
+	"translate": doTranslate,
+	"build":     doBuild,
+}
+
+func showCommands() {
+	fmt.Println("Available commands:")
+	for cmd := range commandMap {
+		fmt.Printf(" - %s\n", cmd)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() <= 0 {
+		flag.Usage()
+		showCommands()
+		return
+	}
+
+	command := flag.Arg(0)
+	entry, found := commandMap[command]
+	if !found {
+		fmt.Printf("Unknown command: %s\n", command)
+		showCommands()
+		return
+	}
+
+	args := flag.Args()[1:]
+	err := entry(args)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
 }
