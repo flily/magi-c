b23db0ecc101c6044733c95b8b8b4f8194f61d6c
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-03-05 11:39:45 +0800

    refactor, move basic elements into basic.go

---
+++ coder/csyntax/basic.go
@@ -0,0 +1,71 @@
+package csyntax
+
+import "fmt"
+
+type CStandard int
+
+const (
+	C89 CStandard = iota
+	C99
+
+	EOLCR   = "\r"
+	EOLLF   = "\n"
+	EOLCRLF = "\r\n"
+)
+
+type StyleBoolean bool
+
+func (b StyleBoolean) Not() StyleBoolean {
+	return !b
+}
+
+func (b StyleBoolean) Select(value CodeElement) CodeElement {
+	if b {
+		return value
+	}
+
+	return DelimiterNone
+}
+
+type StringElement string
+
+func NewIntegerStringElement(i int) StringElement {
+	return StringElement(fmt.Sprintf("%d", i))
+}
+
+func FormatStringElement(format string, args ...any) StringElement {
+	s := fmt.Sprintf(format, args...)
+	return StringElement(s)
+}
+
+func (e StringElement) codeElement() {}
+
+func (e StringElement) String() string {
+	return string(e)
+}
+
+func (e StringElement) ItemString() string {
+	return e.String()
+}
+
+type DelimiterCharacter string
+
+func NewDelimiter(c string) DelimiterCharacter {
+	return DelimiterCharacter(c)
+}
+
+func (d DelimiterCharacter) codeElement() {}
+
+func (d DelimiterCharacter) String() string {
+	return string(d)
+}
+
+func (d DelimiterCharacter) ItemString() string {
+	return d.String()
+}
+
+const (
+	DelimiterNone    StringElement      = ""
+	DelimiterSpace   DelimiterCharacter = " "
+	DefaultDelimiter DelimiterCharacter = " "
+)
--- coder/csyntax/coder.go
+++ coder/csyntax/coder.go
@@ -10,31 +10,6 @@ import (
 	"github.com/flily/magi-c/context"
 )
 
-type CStandard int
-
-const (
-	C89 CStandard = iota
-	C99
-
-	EOLCR   = "\r"
-	EOLLF   = "\n"
-	EOLCRLF = "\r\n"
-)
-
-type StyleBoolean bool
-
-func (b StyleBoolean) Not() StyleBoolean {
-	return !b
-}
-
-func (b StyleBoolean) Select(value CodeElement) CodeElement {
-	if b {
-		return value
-	}
-
-	return DelimiterNone
-}
-
 type CodeStyle struct {
 	Indent                 StringElement
 	FunctionBraceOnNewLine StyleBoolean
@@ -258,49 +233,6 @@ type WritableCollection interface {
 	CodeElement
 	Write(out *StyleWriter, level int) error
 }
-
-type StringElement string
-
-func NewIntegerStringElement(i int) StringElement {
-	return StringElement(fmt.Sprintf("%d", i))
-}
-
-func FormatStringElement(format string, args ...any) StringElement {
-	s := fmt.Sprintf(format, args...)
-	return StringElement(s)
-}
-
-func (e StringElement) codeElement() {}
-
-func (e StringElement) String() string {
-	return string(e)
-}
-
-func (e StringElement) ItemString() string {
-	return e.String()
-}
-
-type DelimiterCharacter string
-
-func NewDelimiter(c string) DelimiterCharacter {
-	return DelimiterCharacter(c)
-}
-
-func (d DelimiterCharacter) codeElement() {}
-
-func (d DelimiterCharacter) String() string {
-	return string(d)
-}
-
-func (d DelimiterCharacter) ItemString() string {
-	return d.String()
-}
-
-const (
-	DelimiterNone    StringElement      = ""
-	DelimiterSpace   DelimiterCharacter = " "
-	DefaultDelimiter DelimiterCharacter = " "
-)
 
 type StyleWriter struct {
 	out              io.StringWriter
